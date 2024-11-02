29f6bd3614ce0022c753f2db3e1d4323c230523f
haikalbintang <mhaikalbintang.work@gmail.com>
2024-11-03 06:37:49 +0700

    Fix: Fix minor.

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
             }
           />
           <Route
-            path="/home"
+            path="/"
             element={
               <ProtectedRoute>
                 <HomePage selectedLink="home" />
