81dbfcca88c9888ca5e210ef099a3637f3ca4282
Haikal Bintang <mhaikalbintang.work@gmail.com>
2024-11-25 15:13:20 +0700

    Edit balance.

--- my-project/src/components/3_modules/Balance.tsx
+++ my-project/src/components/3_modules/Balance.tsx
@@ -6,13 +6,14 @@ interface BalanceProps {
   balanceData: BalanceType;
 }
 
-export default function Balance({ isLoading, balanceData }) {
+export default function Balance({ isLoading, balanceData }: BalanceProps) {
+  let balance = balanceData.balance.toLocaleString();
   return (
     <div className="w-3/5 bg-[url('/BackgroundSaldo.png')] bg-left bg-no-repeat bg-contain text-white h-full">
       <div className="w-full h-full flex flex-col justify-between p-9">
         <h4 className="text-lg">Saldo anda</h4>
         <p className="text-5xl font-semibold tracking-wider pb-3">
-          Rp {isLoading ? "loading..." : `${balanceData.balance}`}
+          Rp {isLoading ? "loading..." : balance}
         </p>
         <h5 className="text-sm">Lihat Saldo</h5>
       </div>
